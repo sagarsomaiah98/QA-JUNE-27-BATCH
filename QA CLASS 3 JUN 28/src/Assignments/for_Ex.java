e41e22e8d5ce13810407689e56bf08239632053a
Som <somaiah1988@gmail.com>
2022-06-29 09:26:06 -0400

    29

---
+++ QA CLASS 3 JUN 28/src/Assignments/for_Ex.java
@@ -0,0 +1,18 @@
+package Assignments;
+
+public class for_Ex {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i=0;i<5;i++) {
+			for(int j=0;j<=i;j++) {
+				
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
