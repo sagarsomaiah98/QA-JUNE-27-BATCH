ec9731d4e82d20d6ff45d6633e7ed22b83654cf6
Som <somaiah1988@gmail.com>
2022-07-07 08:56:59 -0400

    07

---
+++ QA CLASS 6 JUL 5/src/com/emonics/parameter/FindAge.java
@@ -0,0 +1,19 @@
+package com.emonics.parameter;
+
+public class FindAge {
+	
+	
+	public void findage(int day, int month, int year) {
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		FindAge f= new FindAge();
+		f.findage(20,10,1990);
+		//31 years 8 months 17 days
+
+	}
+
+}
